18f1bc5e76dabd796267751c51751475902bc6c1
Hadson Roque <nineonezero.light@gmail.com>
2023-05-16 14:43:51 +0200

    CREATE PhotoContext for database

---
+++ net-il-mio-fotoalbum/Models/PhotoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class PhotoContext : DbContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=galleryDB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+    }
+}
