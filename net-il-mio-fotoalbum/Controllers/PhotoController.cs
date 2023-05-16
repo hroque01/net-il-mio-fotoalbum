e2f5b50b7890fcf9feba12105df41d1ce67759b3
Hadson Roque <nineonezero.light@gmail.com>
2023-05-16 15:54:18 +0200

    Create HTTPGET For Create

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using net_il_mio_fotoalbum.Models;
+using System.Data;
 
 namespace net_il_mio_fotoalbum.Controllers
 {
@@ -10,15 +13,31 @@ namespace net_il_mio_fotoalbum.Controllers
             return View();
         }
 
+        [Authorize(Roles = "ADMIN")]
+        [HttpGet]
         // Controller for CREATE PAGES
         public IActionResult Create()
         {
             //DATABASE
             using (PhotoContext db = new PhotoContext())
             {
+                PhotoFormModel model = new PhotoFormModel();
+                
+                List<SelectListItem> listCategory = new List<SelectListItem>();
+                foreach (Category category in db.Categories)
+                {
+                    listCategory.Add(new SelectListItem()
+                    {
+                        Text = category.Name, Value = category.Id.ToString()
+                    });
+                }
 
+                model.Photo = new Photo();
+                model.Category = listCategory;
+
+                return View("Create", model);
             }
-            return View();
         }
+            
     }
 }
