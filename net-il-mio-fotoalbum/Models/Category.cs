88ea0062432bc107c41c21ca710cc17e60f021e6
Hadson Roque <nineonezero.light@gmail.com>
2023-05-16 14:32:22 +0200

    CREATE classes CATEGORY and PHOTO

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("category")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }  
+
+        // many to many relationship with PHOTO
+        List<Photo> Photos { get; set; }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("photo")]
+    public class Photo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public byte[] Image { get; set; }
+        [Required]
+        public bool visible { get; set; }
+
+        // many to many relationship with Category
+        List<Category> Categories { get; set; } 
+    }
+}
